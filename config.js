08f332933dba5dbf1a35c2f07e1e8e0c2f064585
mfreida <maifreida1@gmail.com>
2018-10-19 15:11:32 +0300

    First commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/217fc73e-d244-492f-bba3-7b1a4c151d34/token";
+const instanceLocator = "v1:us1:217fc73e-d244-492f-bba3-7b1a4c151d34";
+
+export { tokenUrl, instanceLocator }
